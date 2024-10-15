9490c7bfd2b90a14c4339cffb4b28183b573ec99
PieterNatanael <pieternatanael@icloud.com>
2024-10-15 09:35:08 +0800

    Initial Commit

---
+++ Take Medication/Take_MedicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Take_MedicationApp.swift
+//  Take Medication
+//
+//  Created by Pieter Yoshua Natanael on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Take_MedicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Take MedicationTests/Take_MedicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Take_MedicationTests.swift
+//  Take MedicationTests
+//
+//  Created by Pieter Yoshua Natanael on 15/10/24.
+//
+
+import Testing
+@testable import Take_Medication
+
+struct Take_MedicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
